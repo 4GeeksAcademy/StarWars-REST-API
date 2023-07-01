1688033cda8a3174166c7ec8cc2241026679634f
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-07-01 20:28:46 +0000

    Creados endpoints para planetas y personajes

--- src/app.py
+++ src/app.py
@@ -70,6 +70,53 @@ def get_all_people():
         return jsonify([people.serialize() for people in all_people]), 200
     else:
         return jsonify({"message": "People not found"}), 404
+    
+@app.route('/people/<int:id>', methods=['GET'])
+def get_one_people(id):
+    people = People.query.get(id)
+
+    if people is not None:
+        return jsonify(people.serialize()), 200
+    else:
+        return jsonify({"message": "That people hasn't been found"}), 404
+    
+@app.route('/planet', methods=['POST'])
+def post_planet():
+    body = request.json
+
+    if 'name' not in body:
+        return jsonify({"message": "Error no se ha enviando 'name' en el body"}), 400
+    if 'description' not in body:
+        return jsonify({"message": "Error no se ha enviando 'description' en el body"}), 400
+    try:
+        new_planet = Planet(body['name'], body['description'])
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(new_planet.serialize()), 200
+
+    except Exception as err:
+        return jsonify({"message": err}), 500
+    
+
+@app.route('/planets', methods=['GET'])
+def get_all_planet():
+    all_planets = Planet.query.all()
+    
+    if all_planets is not None:
+        return jsonify([planet.serialize() for planet in all_planets]), 200
+    else:
+        return jsonify({"message": "People not found"}), 404
+    
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_one_planet(id):
+    planet = Planet.query.get(id)
+
+    if planet is not None:
+        return jsonify(planet.serialize()), 200
+    else: 
+        return jsonify({"message": "That planet hasn't been found"}), 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
